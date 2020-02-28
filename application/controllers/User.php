45f735307b2e50dabcb3b51195b073d15d437a6e
ariefhdt <blackconnor78@gmail.com>
2020-02-28 17:56:11 +0700

    memperbaiki update foto profile

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -7,6 +7,7 @@ class User extends CI_Controller
     {
         parent::__construct();
         is_logged_in();
+        $this->load->model('MUser');
     }
 
     public function index()
@@ -26,48 +27,21 @@ class User extends CI_Controller
     {
         $data['title'] = 'Edit Profile';
         $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+        $id = $this->session->userdata('id');
 
-        $this->form_validation->set_rules('name', 'Full Name', 'required|trim');
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/sidebar', $data);
+        $this->load->view('templates/topbar', $data);
+        $this->load->view('user/edit', $data);
+        $this->load->view('templates/footer');
 
-        if ($this->form_validation->run() == false) {
-            $this->load->view('templates/header', $data);
-            $this->load->view('templates/sidebar', $data);
-            $this->load->view('templates/topbar', $data);
-            $this->load->view('user/edit', $data);
-            $this->load->view('templates/footer');
-        } else {
-            $name = $this->input->post('name');
-            $email = $this->input->post('email');
-
-            // cek jika ada gambar yang akan diupload
-            $upload_image = $_FILES['image']['name'];
-
-            if ($upload_image) {
-                $config['allowed_types'] = 'gif|jpg|png';
-                $config['max_size']      = '2048';
-                $config['upload_path'] = './assets/img/profile/';
-
-                $this->load->library('upload', $config);
-
-                if ($this->upload->do_upload('image')) {
-                    $old_image = $data['user']['image'];
-                    if ($old_image != 'default.jpg') {
-                        unlink(FCPATH . 'assets/img/profile/' . $old_image);
-                    }
-                    $new_image = $this->upload->data('file_name');
-                    $this->db->set('image', $new_image);
-                } else {
-                    echo $this->upload->display_errors();
-                }
-            }
-
-            $this->db->set('name', $name);
-            $this->db->where('email', $email);
-            $this->db->update('user');
-
-            $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Your profile has been updated!</div>');
-            redirect('user');
+        if (isset($_POST['update_profile'])) {
+            $this->MUser->update_profile($_POST, $id);
+            redirect('user');  
         }
+
+        // $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Your profile has been updated!</div>');
+        //     redirect('user');
     }
 
 
--- application/views/user/edit.php
+++ application/views/user/edit.php
@@ -8,7 +8,8 @@
     <div class="row">
         <div class="col-lg-8">
 
-            <?= form_open_multipart('user/edit'); ?>
+            <!-- <?= form_open_multipart('user/edit'); ?> -->
+            <form method="post" enctype="multipart/form-data">
             <div class="form-group row">
                 <label for="email" class="col-sm-2 col-form-label">Email</label>
                 <div class="col-sm-10">
@@ -20,6 +21,11 @@
                 <div class="col-sm-10">
                     <input type="text" class="form-control" id="name" name="name" value="<?= $user['name']; ?>">
                     <?= form_error('name', '<small class="text-danger pl-3">', '</small>'); ?>
+                </div>
+            </div>
+            <div class="form-group row">
+                <div class="col-sm-10">
+                    <input type="hidden" class="form-control" id="old_image" name="old_image" value="<?= base_url('assets/img/profile/') . $user['image']; ?>">
                 </div>
             </div>
             <div class="form-group row">
@@ -41,7 +47,7 @@
 
             <div class="form-group row justify-content-end">
                 <div class="col-sm-10">
-                    <button type="submit" class="btn btn-primary">Update</button>
+                    <button name="update_profile" type="submit" class="btn btn-primary">Update</button>
                 </div>
             </div>
 
